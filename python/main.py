89f07951eedbb92c1708a86dc5c219206ee0bad8
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2019-10-27 14:15:19 +0300

    Fix python 3.5

--- python/main.py
+++ python/main.py
@@ -27,7 +27,7 @@ def main() -> None:
     text = 'To be, or not to be?'
     counts = word_counts(text)
     sorted_items = sort_by_values_and_keys(counts, reverse=True)
-    print(f'Words count in sentence: "{text}"')
+    print('Words count in sentence: "{text}"'.format(text=text))
     for word, count in sorted_items:
         print(word, count)
 
